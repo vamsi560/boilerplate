a57f8842353f223810af84b353529cfd2446e37f
vamsi560 <145781997+vamsi560@users.noreply.github.com>
2024-09-11 19:28:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,22 @@ provider "aws" {
   region = "us-west-2"  # Specify your desired region
 }
 
+variable "example_variable" {
+  type = string
+  # Missing description
+}
+
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name"  # Replace with your unique bucket name
-  acl    = "private"  # Set the access control list
+  bucket = "hardcoded-bucket-name"  # Hardcoded value
+  acl    = "private"
 
   tags = {
     Name        = "MyBucket"
     Environment = "Dev"
   }
-} 
+}
+
+output "bucket_id" {
+  value = aws_s3_bucket.my_bucket.id
+  # Missing description
+}
